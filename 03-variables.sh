ffc4984553163a8e2fd1ed30e2b3e28d6e741625
Teejay12 <tjshankis@yahoo.com>
2021-03-23 21:01:02 -0400

    Variables commit

---
+++ 03-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Variable is the name you give to certain set of data
+
+## Without variables
+echo welcome to Devops training
+echo training of Devops lasts for 70 days
+echo "Devops training covers shell scripting & Ansible"
+
+## Declaring a variable, Syntax: var=data
+
+COURSE=Devops
+echo welcome to ${COURSE} training
+echo training of $COURSE lasts for 70 days
+echo "$COURSE training covers shell scripting & Ansible"
