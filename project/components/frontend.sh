1235dba1d1ad6afffdbaa3fcc25a9b0d26eeec77
Teejay12 <tjshankis@yahoo.com>
2021-03-25 18:33:50 -0400

    committing nginx installation

--- project/components/frontend.sh
+++ project/components/frontend.sh
@@ -15,6 +15,15 @@ echo "................................................"
 exit
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+if [ $? -ne 0 ]; then
+  echo -e "\e[1;31m [ERROR] Nginx Installation is failure\e[0m"
+  exit2
+else
+  echo -e "\e[1;32m [SUCC] Nginx Installation is successful"
+fi
+echo "................................................"
+echo "................................................"
+exit
 
 cd /usr/share/nginx/html
 rm -rf *
