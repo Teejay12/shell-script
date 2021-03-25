a7c420273859afec5024a28d3272d5a5128a5aeb
Teejay12 <tjshankis@yahoo.com>
2021-03-25 18:31:11 -0400

    committing nginx installation

--- project/components/frontend.sh
+++ project/components/frontend.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+echo -e "\e[1;35m [INFO] Installing Nginx\e[0m"
+echo "................................................"
+echo "................................................"
 yum install nginx -y
-
-systemctl enable nginx
-systemctl start nginx
+if [ $? -ne 0 ]; then
+  echo -e "\e[1;31m [ERROR] Nginx Installation is failure\e[0m"
+  exit2
+else
+  echo -e "\e[1;32m [SUCC] Nginx Installation is successful"
+fi
+echo "................................................"
+echo "................................................"
+exit
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
@@ -16,3 +25,4 @@ rm -rf frontend-master README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 systemctl restart nginx
+systemctl enable nginx
