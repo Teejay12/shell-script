ee5da7f2be60d0655ab15e4e9c3b9e5b355cbe3d
Teejay12 <tjshankis@yahoo.com>
2021-03-27 22:38:31 -0400

    Creating Instances

--- project/instance-create/create.sh
+++ project/instance-create/create.sh
@@ -3,4 +3,11 @@
 
 LID=lt-0985b6451cb03d08d
 LVER=3
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} | jq
+COMPONENT=$1
+
+if [ -z "${COMPONENT}" ]; then
+  echo "Component Name INput is needed"
+  exit1
+fi
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
