22e768b5b6d59107523d54ea8ed372302a5bdf86
Teejay12 <tjshankis@yahoo.com>
2021-03-30 20:55:31 -0400

    mongodb push

--- project/components/mongodb.sh
+++ project/components/mongodb.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 
 source components/common.sh
+
+PRINT "Setup MongoDB Repository"
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+STAT $? "Setup MongoDB Repository"
+
+PRINT "Install MongoDB"
+yum install -y mongodb-org
+STAT $? "Installation of MongoDB"
+
+PRINT  "Update MongoDB Configuration File"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+STAT $? "Updating MongoDB Configuration File"
+
+PRINT "Start MongoDB Service"
+systemctl enable mongod
+systemctl restart mongod
+STAT $? "Starting MongoDB"
+
+PRINT "Download MongoDB Schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+STAT $? "Downloading Schema"
+
+PRINT "Extract MongoDB Schemas"
+cd /tmp
+unzip mongodb.zip
+
+PRINT "Load Schema"
+cd mongodb-main
+mongo < catalogue.js
+mongo < users.js
+STAT $? "Loading Schema"
